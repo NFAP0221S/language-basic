9a396641b206e8b7e2d5d337b8fbc6c9f80695f1
hslee <vawav11@gmail.com>
2024-04-10 22:41:28 +0900

    commit

--- dart/11.positional_parameter_vs _named_parameter/1_index.dart
+++ dart/11.positional_parameter_vs _named_parameter/1_index.dart
@@ -1,3 +1,5 @@
-// List와 Map은 Dart에서 가장 많이 사용되는 컬렉션 타입입니다.
-List<String> names = ['Alice', 'Bob', 'Charlie'];
-Map<String, int> ages = {'Alice': 30, 'Bob': 25};
+// Dart에서 함수는 객체입니다. 익명 함수를 만들거나, 메서드를 정의할 수 있습니다.
+// 아래는 함수 이기 도 하고, class 에서 선언되면, 메서드 입니다.
+void greet(String name) {
+  print('Hello, $name!');
+}
--- dart/2.Variable&DataType/1_index.dart
+++ dart/2.Variable&DataType/1_index.dart
@@ -1,6 +1,10 @@
-// Dart 프로그램은 main() 함수로 시작합니다. 
-// 가장 기본적인 "Hello World" 프로그램은 다음과 같습니다
+// Dart는 타입 추론을 지원하며, var, final, const를 사용해 변수를 선언할 수 있습니다. 
+// 명시적으로 타입을 지정할 수도 있습니다.
 
-void main() {
-  print('Hello, World!')
-}
+var name = 'Dart'; // 타입 추론을 사용
+
+String language = 'Dart'; // 명시적으로 타입 지정
+
+final String version = '2.12'; // 변경할 수 없는 변수
+
+const int max = 10; // 컴파일 타임 상수
